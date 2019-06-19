3bddcb9435f3a7a8c87cfcd4fe62a24719e298ad
Marius van der Wijden <marius@perun.network>
2019-06-19 11:51:46 +0200

    :seedling: common: added marshaling to Address + PubKey
    Signed-off-by: Marius van der Wijden <marius@perun.network>

--- common/types.go
+++ common/types.go
@@ -2,21 +2,27 @@
 // This file is part of go-perun. Use of this source code is governed by a
 // MIT-style license that can be found in the LICENSE file.
 
-// Package common provides type abstractions that are used throughout go-perun
+// Package common provides type abstractions that are used throughout go-perun.
 package common
 
-// Address represents a identifier used in a cryptocurrency
-// It is dependent on the currency and needs to be implemented for every blockchain
+import "encoding"
+
+// Address represents a identifier used in a cryptocurrency.
+// It is dependent on the currency and needs to be implemented for every blockchain.
 type Address interface {
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
 }
 
 // PubKey represents an unabridged public key
-// It is dependent on the currency and needs to be implemented for every blockchain
+// It is dependent on the currency and needs to be implemented for every blockchain.
 type PubKey interface {
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
 
-	// ToAddress converts this public key to an address in the respective cryptocurrency
+	// ToAddress converts this public key to an address in the respective cryptocurrency.
 	ToAddress() Address
 
-	// VerifySign verifies whether a signature was signed by the corresponding sk to this pk
+	// VerifySign verifies whether a signature was signed by the corresponding sk to this pk.
 	VerifySign(sign []byte) error
 }
