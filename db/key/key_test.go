8282fd68768358a57086739e4b9416ac0dfb351d
Sebastian Stammler <seb@perun.network>
2019-07-10 17:35:47 +0200

    :white_check_mark: test key incremental functions
    Signed-off-by: Sebastian Stammler <seb@perun.network>

---
+++ db/key/key_test.go
@@ -0,0 +1,29 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+package key_test
+
+import (
+	"testing"
+
+	"perun.network/go-perun/db/key"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNext(t *testing.T) {
+	assert.Equal(t, key.Next(""), "\x00")
+	assert.Equal(t, key.Next("a"), "a\x00")
+}
+
+func TestIncPrefix(t *testing.T) {
+	assert.Equal(t, key.IncPrefix(""), "")
+	assert.Equal(t, key.IncPrefix("\x00"), "\x01")
+	assert.Equal(t, key.IncPrefix("a"), "b")
+	assert.Equal(t, key.IncPrefix("zoo"), "zop")
+	assert.Equal(t, key.IncPrefix("\xff"), "")
+	assert.Equal(t, key.IncPrefix("\xffa"), "\xffb")
+	assert.Equal(t, key.IncPrefix("a\xff"), "b")
+	assert.Equal(t, key.IncPrefix("\xff\xff\xff"), "")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module perun.network/go-perun
 
 go 1.12
 
-require github.com/pkg/errors v0.8.1
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.3.0
+)
