e167c11f0e79ae2dbddad1bd5a186ed0addef89d
Marius van der Wijden <marius@perun.network>
2019-07-11 20:43:33 +0000

    :pencil: [db] [log] added proper comments, fixed linter errors
    Signed-off-by: Marius van der Wijden <marius@perun.network>

--- db/test/batchtest.go
+++ db/test/batchtest.go
@@ -6,9 +6,7 @@ import (
 	"perun.network/go-perun/db"
 )
 
-/*
-	GenericBatchTest is to be called from the batch implementation tests.
-*/
+// GenericBatchTest is to be called from the batch implementation tests.
 func GenericBatchTest(t *testing.T, database db.Database) {
 	this := BatchTest{T: t, Batch: database.NewBatch()}
 
@@ -17,8 +15,8 @@ func GenericBatchTest(t *testing.T, database db.Database) {
 	dbtest.Put("1234", "1234 initial value")
 	dbtest.Put("5678", "5678 initial value")
 
-	const str_len_1 = "Test Put() tracking."
-	const str_len_2 = "Test Put() tracking override."
+	const strLen1 = "Test Put() tracking."
+	const strLen2 = "Test Put() tracking override."
 
 	this.Batch.Reset()
 	// Batch must be empty after reset.
@@ -30,17 +28,17 @@ func GenericBatchTest(t *testing.T, database db.Database) {
 	this.Batch.Reset()
 	// Put must work on empty and full batches.
 	// New Put() must increase length.
-	this.MustPut("1234", str_len_1)
+	this.MustPut("1234", strLen1)
 	this.MustHaveLength(1)
-	this.MustHaveValueSize(uint(len(str_len_1)))
+	this.MustHaveValueSize(uint(len(strLen1)))
 	// Test PutBytes() overwrite.
-	this.MustPutBytes("1234", []byte(str_len_1))
+	this.MustPutBytes("1234", []byte(strLen1))
 	this.MustHaveLength(1)
-	this.MustHaveValueSize(uint(len(str_len_1)))
+	this.MustHaveValueSize(uint(len(strLen1)))
 	// Overwrite Put() must keep length.
-	this.MustPut("1234", str_len_2)
+	this.MustPut("1234", strLen2)
 	this.MustHaveLength(1)
-	this.MustHaveValueSize(uint(len(str_len_2)))
+	this.MustHaveValueSize(uint(len(strLen2)))
 	// New Delete() must increase length.
 	this.MustDelete("5678")
 	this.MustHaveLength(2)
@@ -61,48 +59,55 @@ func GenericBatchTest(t *testing.T, database db.Database) {
 	return
 }
 
+// BatchTest tests a batch.
 type BatchTest struct {
 	*testing.T
 	Batch db.Batch
 }
 
-func (this *BatchTest) MustPut(key, value string) {
-	if err := this.Batch.Put(key, value); err != nil {
-		this.Fatalf("Put(): Failed to put [%q] = %q: %v.\n", key, value, err)
+// MustPut tests the put functionality.
+func (bt *BatchTest) MustPut(key, value string) {
+	if err := bt.Batch.Put(key, value); err != nil {
+		bt.Fatalf("Put(): Failed to put [%q] = %q: %v.\n", key, value, err)
 	}
 	return
 }
 
-func (this *BatchTest) MustPutBytes(key string, value []byte) {
-	if err := this.Batch.PutBytes(key, value); err != nil {
-		this.Fatalf("PutBytes(): Failed to put [%q] = '%v': %v\n", key, value, err)
+// MustPutBytes tests the putBytes functionality.
+func (bt *BatchTest) MustPutBytes(key string, value []byte) {
+	if err := bt.Batch.PutBytes(key, value); err != nil {
+		bt.Fatalf("PutBytes(): Failed to put [%q] = '%v': %v\n", key, value, err)
 	}
 	return
 }
 
-func (this *BatchTest) MustDelete(key string) {
-	if err := this.Batch.Delete(key); err != nil {
-		this.Fatalf("Put(): Failed to delete [%q]: %v.\n", key, err)
+// MustDelete tests the delete functionality.
+func (bt *BatchTest) MustDelete(key string) {
+	if err := bt.Batch.Delete(key); err != nil {
+		bt.Fatalf("Put(): Failed to delete [%q]: %v.\n", key, err)
 	}
 	return
 }
 
-func (this *BatchTest) MustHaveLength(length uint) {
-	if actual := this.Batch.Len(); actual != length {
-		this.Errorf("Len(): Batch has %d elements, expected %d.\n", actual, length)
+// MustHaveLength tests the len functionality.
+func (bt *BatchTest) MustHaveLength(length uint) {
+	if actual := bt.Batch.Len(); actual != length {
+		bt.Errorf("Len(): Batch has %d elements, expected %d.\n", actual, length)
 	}
 	return
 }
 
-func (this *BatchTest) MustHaveValueSize(size uint) {
-	if actual := this.Batch.ValueSize(); actual != size {
-		this.Errorf("ValueSize(): Batch has size %d, expected %d.\n", actual, size)
+// MustHaveValueSize tests the valueSize functionality.
+func (bt *BatchTest) MustHaveValueSize(size uint) {
+	if actual := bt.Batch.ValueSize(); actual != size {
+		bt.Errorf("ValueSize(): Batch has size %d, expected %d.\n", actual, size)
 	}
 	return
 }
 
-func (this *BatchTest) MustApply() {
-	if err := this.Batch.Apply(); err != nil {
-		this.Errorf("Apply(): Failed with reason %v.\n", err)
+// MustApply tests the apply functionality.
+func (bt *BatchTest) MustApply() {
+	if err := bt.Batch.Apply(); err != nil {
+		bt.Errorf("Apply(): Failed with reason %v.\n", err)
 	}
 }
