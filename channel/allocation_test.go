11cd6ef9d6f1a7da28d09d8fb05367bcf1a8c017
Sebastian Stammler <seb@perun.network>
2019-07-18 15:07:13 +0200

    :white_check_mark: [channel] test equalSum()
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- channel/allocation_test.go
+++ channel/allocation_test.go
@@ -7,6 +7,8 @@ package channel
 import (
 	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAllocation_Sum(t *testing.T) {
@@ -178,4 +180,49 @@ func TestAllocation_valid(t *testing.T) {
 			}
 		})
 	}
+}
+
+// simple summer for testing
+type balsum struct {
+	b []Bal
+}
+
+func (b balsum) Sum() []Bal {
+	return b.b
+}
+
+func TestEqualBalance(t *testing.T) {
+	empty := balsum{make([]Bal, 0)}
+	one1 := balsum{[]Bal{big.NewInt(1)}}
+	one2 := balsum{[]Bal{big.NewInt(2)}}
+	two12 := balsum{[]Bal{big.NewInt(1), big.NewInt(2)}}
+	two48 := balsum{[]Bal{big.NewInt(4), big.NewInt(8)}}
+
+	assert := assert.New(t)
+
+	_, err := equalSum(empty, one1)
+	assert.NotNil(err)
+
+	eq, err := equalSum(empty, empty)
+	assert.Nil(err)
+	assert.True(eq)
+
+	eq, err = equalSum(one1, one1)
+	assert.Nil(err)
+	assert.True(eq)
+
+	eq, err = equalSum(one1, one2)
+	assert.Nil(err)
+	assert.False(eq)
+
+	_, err = equalSum(one1, two12)
+	assert.NotNil(err)
+
+	eq, err = equalSum(two12, two12)
+	assert.Nil(err)
+	assert.True(eq)
+
+	eq, err = equalSum(two12, two48)
+	assert.Nil(err)
+	assert.False(eq)
 }
