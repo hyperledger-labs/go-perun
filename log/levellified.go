9f2bfaaab3dd286cda5b1ddedbb965bcfe5d7114
Steffen Rattay <steffen@perun.network>
2019-07-24 18:27:44 +0000

    :sparkling: Added global logger functions.
    This had the consequence that the log.Level enum constants
    had to be renamed to (…)Level.  
      
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- log/levellified.go
+++ log/levellified.go
@@ -7,14 +7,14 @@ package log
 import "log"
 
 // Default level shift, such that Level value 0 is the default log level
-const defaultLogLevelShift = 3 // Warn
+const defaultLogLevelShift = 3 // WarnLevel
 
 // compile-time check that Levellified extends a StdLogger to a LevelLogger
 var _ LevelLogger = &Levellified{StdLogger: &log.Logger{}}
 
 // Levellified levellifies a standard logger. Calls are just forwarded to the wrapped
 // logger's Print{,f,ln} methods with the prefix [level], except for levels
-// Panic and Fatal, which are forwarded to the respective methods.
+// PanicLevel and FatalLevel, which are forwarded to the respective methods.
 type Levellified struct {
 	// wrapped logger
 	StdLogger
@@ -26,24 +26,24 @@ type Levellified struct {
 type Level int8
 
 const (
-	// Fatal calls the wrapped logger's Fatal method with the prefix "[fatal]".
+	// FatalLevel calls the wrapped logger's Fatal method with the prefix "[fatal]".
 	// The wrapped logger should usually immediately exit the program with
 	// os.Exit(1) or similar.  It it the highest level of severity.
-	Fatal Level = iota - defaultLogLevelShift // -3: default level Warn
-	// Panic calls the wrapped logger's Panic method with the prefix "[panic]".
+	FatalLevel Level = iota - defaultLogLevelShift // -3: default level WarnLevel
+	// PanicLevel calls the wrapped logger's Panic method with the prefix "[panic]".
 	// The wrapped logger should usually call panic with the given message
-	Panic
-	// Error calls the wrapped logger's Print method with the prefix "[error]".
-	Error
-	// Warn calls the wrapped logger's Print method with the prefix "[warn]".
+	PanicLevel
+	// ErrorLevel calls the wrapped logger's Print method with the prefix "[error]".
+	ErrorLevel
+	// WarnLevel calls the wrapped logger's Print method with the prefix "[warn]".
 	// It is the default level.
-	Warn
-	// Info calls the wrapped logger's Print method with the prefix "[info]".
-	Info
-	// Debug calls the wrapped logger's Print method with the prefix "[debug]".
-	Debug
-	// Trace calls the wrapped logger's Print method with the prefix "[trace]".
-	Trace
+	WarnLevel
+	// InfoLevel calls the wrapped logger's Print method with the prefix "[info]".
+	InfoLevel
+	// DebugLevel calls the wrapped logger's Print method with the prefix "[debug]".
+	DebugLevel
+	// TraceLevel calls the wrapped logger's Print method with the prefix "[trace]".
+	TraceLevel
 )
 
 // String returns the string representation.
@@ -53,95 +53,95 @@ func (l Level) String() string {
 
 // Tracef implementents log level trace and format parameters.
 func (l *Levellified) Tracef(format string, args ...interface{}) {
-	l.lprintf(Trace, format, args...)
+	l.lprintf(TraceLevel, format, args...)
 }
 
 // Trace implements log level trace.
 func (l *Levellified) Trace(args ...interface{}) {
-	if l.Lvl >= Trace {
+	if l.Lvl >= TraceLevel {
 		l.StdLogger.Print(prepend("[trace] ", args)...)
 	}
 }
 
 // Traceln implements log.TraceLn with white spaces in between arguments.
 func (l *Levellified) Traceln(args ...interface{}) {
-	if l.Lvl >= Trace {
+	if l.Lvl >= TraceLevel {
 		l.StdLogger.Println(prepend("[trace]", args)...)
 	}
 }
 
 // Debugf implementents log level debug and format parameters.
 func (l *Levellified) Debugf(format string, args ...interface{}) {
-	l.lprintf(Debug, format, args...)
+	l.lprintf(DebugLevel, format, args...)
 }
 
 // Debug implements log level debug.
 func (l *Levellified) Debug(args ...interface{}) {
-	if l.Lvl >= Debug {
+	if l.Lvl >= DebugLevel {
 		l.StdLogger.Print(prepend("[debug] ", args)...)
 	}
 }
 
 // Debugln implements log.Debugln with white spaces in between arguments.
 func (l *Levellified) Debugln(args ...interface{}) {
-	if l.Lvl >= Debug {
+	if l.Lvl >= DebugLevel {
 		l.StdLogger.Println(prepend("[debug]", args)...)
 	}
 }
 
 // Infof implementents log level info and format parameters.
 func (l *Levellified) Infof(format string, args ...interface{}) {
-	l.lprintf(Info, format, args...)
+	l.lprintf(InfoLevel, format, args...)
 }
 
 // Info implements log level info.
 func (l *Levellified) Info(args ...interface{}) {
-	if l.Lvl >= Info {
+	if l.Lvl >= InfoLevel {
 		l.StdLogger.Print(prepend("[info] ", args)...)
 	}
 }
 
 // Infoln implements log.Infoln with white spaces in between arguments.
 func (l *Levellified) Infoln(args ...interface{}) {
-	if l.Lvl >= Info {
+	if l.Lvl >= InfoLevel {
 		l.StdLogger.Println(prepend("[info]", args)...)
 	}
 }
 
 // Warnf implementents log level warn and format parameters.
 func (l *Levellified) Warnf(format string, args ...interface{}) {
-	l.lprintf(Warn, format, args...)
+	l.lprintf(WarnLevel, format, args...)
 }
 
 // Warn implements log level warn.
 func (l *Levellified) Warn(args ...interface{}) {
-	if l.Lvl >= Warn {
+	if l.Lvl >= WarnLevel {
 		l.StdLogger.Print(prepend("[warn] ", args)...)
 	}
 }
 
 // Warnln implements log.Warnln with white spaces in between arguments.
 func (l *Levellified) Warnln(args ...interface{}) {
-	if l.Lvl >= Warn {
+	if l.Lvl >= WarnLevel {
 		l.StdLogger.Println(prepend("[warn]", args)...)
 	}
 }
 
 // Errorf implementents log level error and format parameters.
 func (l *Levellified) Errorf(format string, args ...interface{}) {
-	l.lprintf(Error, format, args...)
+	l.lprintf(ErrorLevel, format, args...)
 }
 
 // Error implements log level error.
 func (l *Levellified) Error(args ...interface{}) {
-	if l.Lvl >= Error {
+	if l.Lvl >= ErrorLevel {
 		l.StdLogger.Print(prepend("[error] ", args)...)
 	}
 }
 
 // Errorln implements log.Errorln with white spaces in between arguments.
 func (l *Levellified) Errorln(args ...interface{}) {
-	if l.Lvl >= Error {
+	if l.Lvl >= ErrorLevel {
 		l.StdLogger.Println(prepend("[error]", args)...)
 	}
 }
--- log/log.go
+++ log/log.go
@@ -20,7 +20,7 @@ var (
 
 	// Log is the framework logger. Framework users should set this variable to
 	// their logger. It is set to the None non-logging logger by default.
-	Log Logger = None
+	Log = None
 )
 
 // StdLogger describes the interface of the standard library log package logger.
@@ -75,3 +75,75 @@ type Logger interface {
 	WithFields(Fields) Logger
 	WithError(error) Logger
 }
+
+// Printf calls Printf on the global Logger object
+func Printf(format string, args ...interface{}) { Log.Printf(format, args) }
+
+// Print calls Print on the global Logger object
+func Print(args ...interface{}) { Log.Print(args) }
+
+// Println calls Println on the global Logger object
+func Println(args ...interface{}) { Log.Println(args) }
+
+// Fatalf calls Fatalf on the global Logger object
+func Fatalf(format string, args ...interface{}) { Log.Fatalf(format, args) }
+
+// Fatal calls Fatal on the global Logger object
+func Fatal(args ...interface{}) { Log.Fatal(args) }
+
+// Fatalln calls Fatalln on the global Logger object
+func Fatalln(args ...interface{}) { Log.Fatalln(args) }
+
+// Panicf calls Panicf on the global Logger object
+func Panicf(format string, args ...interface{}) { Log.Panicf(format, args) }
+
+// Panic calls Panic on the global Logger object
+func Panic(args ...interface{}) { Log.Panic(args) }
+
+// Panicln calls Panicln on the global Logger object
+func Panicln(args ...interface{}) { Log.Panicln(args) }
+
+// Tracef calls Tracef on the global Logger object
+func Tracef(format string, args ...interface{}) { Log.Tracef(format, args) }
+
+// Trace calls Trace on the global Logger object
+func Trace(args ...interface{}) { Log.Trace(args) }
+
+// Traceln calls Traceln on the global Logger object
+func Traceln(args ...interface{}) { Log.Traceln(args) }
+
+// Debugf calls Debugf on the global Logger object
+func Debugf(format string, args ...interface{}) { Log.Debugf(format, args) }
+
+// Debug calls Debug on the global Logger object
+func Debug(args ...interface{}) { Log.Debug(args) }
+
+// Debugln calls Debugln on the global Logger object
+func Debugln(args ...interface{}) { Log.Debugln(args) }
+
+// Infof calls Infof on the global Logger object
+func Infof(format string, args ...interface{}) { Log.Infof(format, args) }
+
+// Info calls Info on the global Logger object
+func Info(args ...interface{}) { Log.Info(args) }
+
+// Infoln calls Infoln on the global Logger object
+func Infoln(args ...interface{}) { Log.Infoln(args) }
+
+// Warnf calls Warnf on the global Logger object
+func Warnf(format string, args ...interface{}) { Log.Warnf(format, args) }
+
+// Warn calls Warn on the global Logger object
+func Warn(args ...interface{}) { Log.Warn(args) }
+
+// Warnln calls Warnln on the global Logger object
+func Warnln(args ...interface{}) { Log.Warnln(args) }
+
+// Errorf calls Errorf on the global Logger object
+func Errorf(format string, args ...interface{}) { Log.Errorf(format, args) }
+
+// Error calls Error on the global Logger object
+func Error(args ...interface{}) { Log.Error(args) }
+
+// Errorln calls Errorln on the global Logger object
+func Errorln(args ...interface{}) { Log.Errorln(args) }
