8052938e3dc420b756b342ffb263226245d499de
Steffen Rattay <steffen@perun.network>
2019-07-29 14:52:37 +0200

    :bug: [pkg/test] Fixed CheckPanic logic.
    Previously, CheckPanic() did not detect panic(nil) calls.
    Now returns (bool, interface{}). Added panic(nil) test case.
    Closes #61.  
      
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- pkg/test/panic.go
+++ pkg/test/panic.go
@@ -6,10 +6,16 @@
 package test // import "perun.network/go-perun/pkg/test"
 
 // CheckPanic tests whether a supplied function panics during its execution.
-// Returns nil if the supplied function did not panic, otherwise, returns the
+// Returns whether the supplied function did panic, and if so, also returns the
 // value passed to panic().
-func CheckPanic(function func()) (value interface{}) {
+func CheckPanic(function func()) (didPanic bool, value interface{}) {
+	// Catch the panic, if it happens and store the passed value.
 	defer func() { value = recover() }()
+	// Set up for the panic case.
+	didPanic = true
+	// Call the function to be checked.
 	function()
+	// This is only executed if panic() was not called.
+	didPanic = false
 	return
 }
--- pkg/test/panic_test.go
+++ pkg/test/panic_test.go
@@ -9,12 +9,17 @@ import "testing"
 func TestCheckPanic(t *testing.T) {
 	// Test whether panic calls are properly detected and whether the supplied
 	// value is also properly recorded.
-	if "panicvalue" != CheckPanic(func() { panic("panicvalue") }) {
+	if p, v := CheckPanic(func() { panic("panicvalue") }); !p || v != "panicvalue" {
 		t.Error("Failed to detect panic!")
 	}
 
+	// Test whether panic(nil) calls are detected.
+	if p, v := CheckPanic(func() { panic(nil) }); !p || v != nil {
+		t.Error("Failed to detect panic(nil)!")
+	}
+
 	// Test whether the absence of panic calls is properly detected.
-	if nil != CheckPanic(func() {}) {
+	if p, v := CheckPanic(func() {}); p || v != nil {
 		t.Error("False positive panic detection!")
 	}
 }
