f12a6bcfe0d2615fb5d06c23eb7b2e0059ec6629
Oliver Tale-Yazdi <oliver@perun.network>
2019-09-13 13:47:52 +0200

    :alien: Added init backend set for packages
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

---
+++ backend/sim/wallet/init.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+package wallet // import "perun.network/go-perun/backend/sim/wallet"
+
+import (
+	"perun.network/go-perun/wallet"
+)
+
+func init() {
+	wallet.SetBackend(new(Backend))
+}
