eaa0b91f3fc5e4c3bc618f0a93730706426e2850
Christoph Conrads <christoph@perun.network>
2019-09-20 13:50:20 +0200

    [pkg/io/test] check decode/encode test termination
    Check if `genericDecodeEncodeTest` terminates if not all bytes in the
    pipe are consumed by the reader.
    This test is not run by `go test` because it should fail but marking a
    test as "should fail" is not possible with the golang built-in test
    tool. :|  
      
    Signed-off-by: Christoph Conrads <christoph@perun.network>

---
+++ pkg/io/test/serializabletest_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"fmt"
+	"io"
+	"testing"
+)
+
+type IncompleteRead struct {
+	xs [2]byte
+}
+
+func (d *IncompleteRead) Encode(w io.Writer) error {
+	_, err := w.Write(d.xs[:])
+	return err
+}
+
+func (d *IncompleteRead) Decode(r io.Reader) error {
+	n, err := r.Read(make([]byte, 1))
+	if n != 1 {
+		return fmt.Errorf("Expected reading %d bytes, read %d", 1, n)
+	}
+	return err
+}
+
+func MaybeTestGenericDecodeEncodeTest_termination_ShouldFail(t *testing.T) {
+	d := IncompleteRead{[2]byte{1, 2}}
+	genericDecodeEncodeTest(t, &d)
+}
