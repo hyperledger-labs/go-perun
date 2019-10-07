93b4c125ac0ba2ed5985116d8c03267eb29b0c07
Oliver Tale-Yazdi <oliver@perun.network>
2019-10-07 18:17:44 +0200

    :alien: Added NoAppBackend
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

--- backend/sim/channel/init.go
+++ backend/sim/channel/init.go
@@ -5,9 +5,27 @@
 package channel // import "perun.network/go-perun/backend/sim/channel"
 
 import (
+	"io"
+
 	"perun.network/go-perun/channel"
+	test "perun.network/go-perun/channel/test"
+	"perun.network/go-perun/wallet"
 )
 
 func init() {
 	channel.SetBackend(new(backend))
+	channel.SetAppBackend(new(noAppBackend))
+}
+
+type noAppBackend struct{}
+
+var _ channel.AppBackend = &noAppBackend{}
+
+func (noAppBackend) AppFromDefinition(addr wallet.Address) (channel.App, error) {
+	return test.NewNoApp(addr), nil
+}
+
+func (noAppBackend) DecodeAsset(r io.Reader) (channel.Asset, error) {
+	var asset test.Asset
+	return &asset, asset.Decode(r)
 }
