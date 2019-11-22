e0971bd4f7278be71d77fae78a3c14c6e8a1be54
Sebastian Stammler <seb@perun.network>
2019-11-22 17:39:01 +0100

    :sparkles: [peer] Add Peer.String() string
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- peer/peer.go
+++ peer/peer.go
@@ -181,3 +181,8 @@ func newPeer(addr Address, conn Conn, _ Dialer) *Peer {
 
 	return p
 }
+
+// String returns the peer's address string
+func (p *Peer) String() string {
+	return p.PerunAddress.String()
+}
