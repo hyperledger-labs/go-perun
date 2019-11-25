78b6ffb1874a4ce6778f3fca380661afa1394d1e
Oliver Tale-Yazdi <oliver@perun.network>
2019-11-25 16:34:07 +0000

    :alien: [client] Used ValidateParameters
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

--- client/proposalmsgs.go
+++ client/proposalmsgs.go
@@ -168,15 +168,13 @@ func (c ChannelProposal) SessID() (sid SessionID) {
 // * No locked sub-allocations
 // * InitBals match the dimension of Parts
 func (c ChannelProposal) Valid() error {
-	if c.ParticipantAddr == nil || c.InitBals == nil || c.Nonce == nil {
+	if c.InitBals == nil || c.ParticipantAddr == nil {
 		return errors.New("invalid nil fields")
-	} else if c.ChallengeDuration == 0 {
-		return errors.New("challenge duration must not be zero")
-	} else if len(c.Parts) < 2 || len(c.Parts) > channel.MaxNumParts {
-		return errors.New("invalid number of participants")
+	} else if err := channel.ValidateParameters(c.ChallengeDuration, len(c.Parts), c.AppDef, c.Nonce); err != nil {
+		return errors.WithMessage(err, "invalid channel parameters")
 	} else if err := c.InitBals.Valid(); err != nil {
 		return err
-	} else if len(c.InitBals.Locked) > 0 {
+	} else if len(c.InitBals.Locked) != 0 {
 		return errors.New("initial allocation cannot have locked funds")
 	} else if len(c.InitBals.OfParts) != len(c.Parts) {
 		return errors.New("wrong dimension of initial balances")
