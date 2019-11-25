f3241cc3d0f606283d00fc8e5fa645137f7e1c96
Oliver Tale-Yazdi <oliver@perun.network>
2019-11-25 14:37:44 +0100

    :sparkles: [channel] Added MockAppBackend
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

---
+++ channel/mock_app_backend.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+package channel // import "perun.network/go-perun/channel"
+
+import (
+	"perun.network/go-perun/wallet"
+)
+
+type MockAppBackend struct{}
+
+var _ AppBackend = &MockAppBackend{}
+
+func (MockAppBackend) AppFromDefinition(addr wallet.Address) (App, error) {
+	return NewMockApp(addr), nil
+}
