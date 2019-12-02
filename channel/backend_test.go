103d092928576861d51d5387a3239db2f118cd96
Oliver Tale-Yazdi <oliver@perun.network>
2019-12-02 13:09:28 +0100

    :white_check_mark: Added +build tags
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

--- channel/backend_test.go
+++ channel/backend_test.go
@@ -2,6 +2,8 @@
 // This file is part of go-perun. Use of this source code is governed by a
 // MIT-style license that can be found in the LICENSE file.
 
+// +build wrap_test
+
 package channel
 
 import (
