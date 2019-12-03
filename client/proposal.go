557a0868a120aa542feb12fd25f884210c06602f
Sebastian Stammler <seb@perun.network>
2019-12-03 22:32:27 +0100

    :recycle: [client] change scope of exchangeChannelProposal
    - renamed to exchangeTwoPartyProposal
    - now only handles the sending of the proposal and receival of response  
      
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- client/proposal.go
+++ client/proposal.go
@@ -200,34 +200,14 @@ func (c *Client) handleChannelProposal(p *peer.Peer, req *ChannelProposalReq) {
 	}
 }
 
-func (c *Client) exchangeChannelProposal(
+func (c *Client) exchangeTwoPartyProposal(
 	ctx context.Context,
 	proposal *ChannelProposalReq,
-) (*channelProposalResult, error) {
-	if err := c.validTwoPartyProposal(proposal, 0, proposal.PeerAddrs[1]); err != nil {
-		return nil, errors.WithMessage(err, "invalid channel proposal")
-	}
-
-	numParts := len(proposal.PeerAddrs)
-	if numParts != 2 {
-		return nil, errors.Errorf(
-			"Expected exactly two peers in proposal, got %d", numParts)
-	}
-
+) ([]wallet.Address, error) {
 	p, err := c.peers.Get(ctx, proposal.PeerAddrs[1])
 	if err != nil {
-		return nil, errors.WithMessage(err, "failed to Get() participant [1]")
+		return nil, errors.WithMessage(err, "failed to Get() participant[1]")
 	}
-
-	app, err := channel.AppFromDefinition(proposal.AppDef)
-	if err != nil {
-		return nil, errors.WithMessagef(
-			err, "Error when getting app at address %v", proposal.AppDef)
-	}
-
-	// begin communication with peer
-	receiver := peer.NewReceiver()
-	defer receiver.Close()
 
 	sessID := proposal.SessID()
 	isResponse := func(m wire.Msg) bool {
@@ -236,9 +216,11 @@ func (c *Client) exchangeChannelProposal(
 			(m.Type() == wire.ChannelProposalRej &&
 				m.(*ChannelProposalRej).SessID == sessID)
 	}
+	receiver := peer.NewReceiver()
+	defer receiver.Close()
+
 	if err := p.Subscribe(receiver, isResponse); err != nil {
-		return nil, errors.WithMessagef(
-			err, "subscription error with peer %v", p.PerunAddress)
+		return nil, errors.WithMessagef(err, "subscribing peer %v", p)
 	}
 
 	if err := p.Send(ctx, proposal); err != nil {
@@ -249,23 +231,12 @@ func (c *Client) exchangeChannelProposal(
 	if rawResponse == nil {
 		return nil, errors.New("timeout when waiting for proposal response")
 	}
-	if rejection, ok := rawResponse.(*ChannelProposalRej); ok {
-		return nil, errors.New(rejection.Reason)
+	if rej, ok := rawResponse.(*ChannelProposalRej); ok {
+		return nil, errors.Errorf("channel proposal rejected: %v", rej.Reason)
 	}
 
-	approval := rawResponse.(*ChannelProposalAcc)
-	partAddrs := []wallet.Address{
-		proposal.ParticipantAddr, approval.ParticipantAddr}
-	params, err := channel.NewParams(
-		proposal.ChallengeDuration, partAddrs, app.Def(), proposal.Nonce,
-	)
-	if err != nil {
-		return nil, errors.WithMessage(
-			err, "error when computing params from channel proposal")
-	}
-
-	return &channelProposalResult{
-		params, proposal.InitData, proposal.InitBals}, nil
+	acc := rawResponse.(*ChannelProposalAcc) // this is safe because of predicate isResponse
+	return []wallet.Address{proposal.ParticipantAddr, acc.ParticipantAddr}, nil
 }
 
 // validTwoPartyProposal checks that the proposal is valid in the two-party
