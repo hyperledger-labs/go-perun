360877a479a9c9bb33e5d6464e7fddee6e15a12d
Sebastian Stammler <seb@perun.network>
2019-12-04 09:21:57 +0100

    :sparkles: [client] add Channel.ID() method
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- client/channel.go
+++ client/channel.go
@@ -52,6 +52,10 @@ func (c *Channel) setLogger(l log.Logger) {
 	c.log = l
 }
 
+func (c *Channel) ID() channel.ID {
+	return c.machine.ID()
+}
+
 // init brings the state machine into the InitSigning phase. It is not callable
 // by the user since the Client initializes the channel controller.
 // The state machine is not locked as this function is expected to be called
