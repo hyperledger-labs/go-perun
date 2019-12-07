448b4ebd487d32725a2ebbc75c8df2a552412e95
Sebastian Stammler <seb@perun.network>
2019-12-07 15:45:49 +0100

    :white_check_mark: [client] fix ChannelMsg tests
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- client/updatemsgs_test.go
+++ client/updatemsgs_test.go
@@ -3,15 +3,15 @@
 // of this source code is governed by a MIT-style license that can be found in
 // the LICENSE file.
 
-package client_test
+package client
 
 import (
 	"math/rand"
 	"testing"
 
 	"perun.network/go-perun/channel/test"
-	"perun.network/go-perun/client"
 	"perun.network/go-perun/wallet"
+	wallettest "perun.network/go-perun/wallet/test"
 	"perun.network/go-perun/wire/msg"
 )
 
@@ -19,11 +19,13 @@ func TestChannelUpdateSerialization(t *testing.T) {
 	rng := rand.New(rand.NewSource(0xdeadbeef))
 	for i := 0; i < 4; i++ {
 		params := test.NewRandomParams(rng, test.NewRandomApp(rng).Def())
-		sig, _ := wallet.DecodeSig(rng)
-		m := &client.ChannelUpdate{
-			State:    test.NewRandomState(rng, params),
-			ActorIdx: uint16(rng.Int31n(int32(len(params.Parts)))),
-			Sig:      sig,
+		sig := newRandomSig(rng)
+		m := &msgChannelUpdate{
+			ChannelUpdate: ChannelUpdate{
+				State:    test.NewRandomState(rng, params),
+				ActorIdx: uint16(rng.Int31n(int32(len(params.Parts)))),
+			},
+			Sig: sig,
 		}
 		msg.TestMsg(t, m)
 	}
@@ -32,8 +34,8 @@ func TestChannelUpdateSerialization(t *testing.T) {
 func TestChannelUpdateAccSerialization(t *testing.T) {
 	rng := rand.New(rand.NewSource(0xc0ffeefee))
 	for i := 0; i < 4; i++ {
-		sig, _ := wallet.DecodeSig(rng)
-		m := &client.ChannelUpdateAcc{
+		sig := newRandomSig(rng)
+		m := &msgChannelUpdateAcc{
 			ChannelID: test.NewRandomChannelID(rng),
 			Version:   uint64(rng.Int63()),
 			Sig:       sig,
@@ -45,14 +47,32 @@ func TestChannelUpdateAccSerialization(t *testing.T) {
 func TestChannelUpdateRejSerialization(t *testing.T) {
 	rng := rand.New(rand.NewSource(0xdeadbeef))
 	for i := 0; i < 4; i++ {
-		params := test.NewRandomParams(rng, test.NewRandomApp(rng).Def())
-		sig, _ := wallet.DecodeSig(rng)
-		m := &client.ChannelUpdateRej{
-			Reason:   newRandomString(rng, 16, 16),
-			Alt:      test.NewRandomState(rng, params),
-			ActorIdx: uint16(rng.Int31n(int32(len(params.Parts)))),
-			Sig:      sig,
+		m := &msgChannelUpdateRej{
+			ChannelID: test.NewRandomChannelID(rng),
+			Version:   uint64(rng.Int63()),
+			Reason:    newRandomString(rng, 16, 16),
 		}
 		msg.TestMsg(t, m)
 	}
+}
+
+// newRandomSig generates a random account and then returns the signature on
+// some random data.
+func newRandomSig(rng *rand.Rand) wallet.Sig {
+	acc := wallettest.NewRandomAccount(rng)
+	data := make([]byte, 8)
+	rng.Read(data)
+	sig, err := acc.SignData(data)
+	if err != nil {
+		panic("signing error")
+	}
+	return sig
+}
+
+// newRandomstring returns a random string of length between minLen and
+// minLen+maxLenDiff.
+func newRandomString(rng *rand.Rand, minLen, maxLenDiff int) string {
+	r := make([]byte, minLen+rng.Intn(maxLenDiff))
+	rng.Read(r)
+	return string(r)
 }
