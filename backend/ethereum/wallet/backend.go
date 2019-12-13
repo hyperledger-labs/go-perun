8a70d6ec02b056d04779b285e6a10cd555d3ac25
Marius van der Wijden <marius@perun.network>
2019-12-13 13:56:24 +0100

    :bug: [eth/wallet] fixed signatures to use prefix, v += 27
    Signed-off-by: Marius van der Wijden <marius@perun.network>

--- backend/ethereum/wallet/backend.go
+++ backend/ethereum/wallet/backend.go
@@ -19,10 +19,10 @@ import (
 // Backend implements the utility interface defined in the wallet package.
 type Backend struct{}
 
-// SignatureLength length of a signature in byte.
+// SigLen length of a signature in byte.
 // ref https://godoc.org/github.com/ethereum/go-ethereum/crypto/secp256k1#Sign
 // ref https://github.com/ethereum/go-ethereum/blob/54b271a86dd748f3b0bcebeaf678dc34e0d6177a/crypto/signature_cgo.go#L66
-const SignatureLength = 65
+const SigLen = 65
 
 // compile-time check that the ethereum backend implements the perun backend
 var _ perun.Backend = (*Backend)(nil)
@@ -60,17 +60,26 @@ func DecodeAddress(r io.Reader) (perun.Address, error) {
 
 // DecodeSig reads a []byte with length of an ethereum signature
 func DecodeSig(r io.Reader) (perun.Sig, error) {
-	buf := make(perun.Sig, SignatureLength)
+	buf := make(perun.Sig, SigLen)
 	return buf, wire.Decode(r, &buf)
 }
 
 // VerifySignature verifies if a signature was made by this account.
 func VerifySignature(msg []byte, sig perun.Sig, a perun.Address) (bool, error) {
-	hash := crypto.Keccak256(msg)
+	hash := prefixedHash(msg)
+	if len(sig) == SigLen && (sig[SigLen-1] == 27 || sig[SigLen-1] == 28) {
+		sig[SigLen-1] = sig[SigLen-1] - 27
+	}
 	pk, err := crypto.SigToPub(hash, sig)
 	if err != nil {
 		return false, err
 	}
 	addr := crypto.PubkeyToAddress(*pk)
 	return a.Equals(&Address{addr}), nil
+}
+
+func prefixedHash(data []byte) []byte {
+	hash := crypto.Keccak256(data)
+	prefix := []byte("\x19Ethereum Signed Message:\n32")
+	return crypto.Keccak256(prefix, hash)
 }
