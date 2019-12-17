ee5d5d4a67e04ba9084d71ee39049c55255459ea
Sebastian Stammler <seb@perun.network>
2019-12-17 21:27:57 +0100

    :page_facing_up: correct copyright statement in go files
    Used command:
    find . -name '*.go' -exec sed -e '/\/\/ Copyright/r .copyright-notice' -e '/\/\/ Copyright/,/file.$/d' -i {} +
    which deletes any old copyright notice and replaces it with the content
    from file .copyright-notice  
      
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- db/leveldb/database.go
+++ db/leveldb/database.go
@@ -1,6 +1,7 @@
-// Copyright (c) 2019 The Perun Authors. All rights reserved.
-// This file is part of go-perun. Use of this source code is governed by a
-// MIT-style license that can be found in the LICENSE file.
+// Copyright (c) 2019 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by a MIT-style license that can be found in
+// the LICENSE file.
 
 package leveldb
 
