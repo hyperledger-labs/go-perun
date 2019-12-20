3fbabef271afcdfa00f68e864681113fdb0474cb
Sebastian Stammler <seb@perun.network>
2019-12-20 11:41:43 +0100

    :white_check_mark: [client] use payment app in tests
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- client/init_test.go
+++ client/init_test.go
@@ -6,15 +6,24 @@
 package client
 
 import (
+	"math/rand"
+
 	"github.com/sirupsen/logrus"
 
+	"perun.network/go-perun/apps/payment"
 	_ "perun.network/go-perun/backend/sim/channel" // backend init
 	_ "perun.network/go-perun/backend/sim/wallet"  // backend init
 	plogrus "perun.network/go-perun/log/logrus"
+	wallettest "perun.network/go-perun/wallet/test"
 )
 
 // This file initializes the blockchain and logging backend for both, whitebox
 // and blackbox tests (files *_test.go in packages client and client_test).
 func init() {
 	plogrus.Set(logrus.WarnLevel, &logrus.TextFormatter{ForceColors: true})
+
+	// Tests of package client use the payment app for now...
+	rng := rand.New(rand.NewSource(0x9a09c3e008f1242))
+	appDef := wallettest.NewRandomAddress(rng)
+	payment.SetAppDef(appDef) // payment app address has to be set once at startup
 }
