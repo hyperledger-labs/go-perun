82c9d567ca7ca653e921db11ae8a254d690c88c3
Sebastian Stammler <seb@perun.network>
2019-12-23 00:29:06 +0100

    :recycle: [pkg/test] improve exit tester impl
    exit function is now only temporarily changed. Also added global wrapper
    calls.  
      
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- pkg/test/exit.go
+++ pkg/test/exit.go
@@ -5,72 +5,102 @@
 
 package test
 
-// Exit can test calls to an exit function, which is usually a package global
-// set to os.Exit by default.
-type Exit struct {
-	t      T
-	called bool
-	code   int
-}
+type (
+	// Exit can test calls to an exit function, which is usually a package global
+	// set to os.Exit by default.
+	Exit struct {
+		t    T
+		exit *func(int)
+	}
 
-// NewExit creates a new Exit tester, setting the passed function exit to its
-// Exit function. This exit should usually be the address of a global exit
-// function that is set to os.Exit by default.
+	// An exiter is used by Exit's Assert methods to record calls to exit.
+	exiter struct {
+		called bool
+		code   int
+	}
+)
+
+// NewExit creates a new Exit tester. The exit function pointer should usually
+// be the address of a global exit function variable that is set to os.Exit by
+// default. The exit function is temporarily modified during Assert tests.
 func NewExit(t T, exit *func(int)) *Exit {
-	et := &Exit{t: t}
-	*exit = et.Exit
-	return et
+	return &Exit{
+		t:    t,
+		exit: exit,
+	}
 }
 
-// Exit mocks exit calls. Usually, this shouldn't need to be called directly as
-// the exit function to patch is set during NewExit().
-func (e *Exit) Exit(code int) {
+// Exit records exit calls.
+func (e *exiter) Exit(code int) {
 	e.called = true
 	e.code = code
 	// panic() to stop the execution of the function, as would a normal call to
 	// os.Exit() do. Unfortunately, we cannot prevent stack unwinding but this is
 	// as close as we can get.
-	panic("Exit.Exit()")
+	panic("exiter.Exit()")
 }
 
-// AssertExit asserts that fn calls e.Exit(). Usually, fn should call a global
-// exit function variable that is set to os.Exit by default.
+// AssertExit asserts that fn calls the exit function that was passed to NewExit
+// with the given code. Usually, exit is a package global function variable that
+// is set to os.Exit by default. The exit function is temporarily modified
+// during the test.
 func (e *Exit) AssertExit(fn func(), code int) {
-	e.assert(func(e *Exit) {
-		if !e.called {
+	e.assert(func(ex *exiter) {
+		if !ex.called {
 			e.t.Error("exit was not called")
-		} else if e.code != code {
-			e.t.Errorf("exit was called with wrong code %d, expected %d", e.code, code)
+		} else if ex.code != code {
+			e.t.Errorf("exit was called with wrong code %d, expected %d", ex.code, code)
 		}
 	}, fn)
 }
 
-// AssertNoExit asserts that fn calls e.Exit(). Usually, fn should call a global
-// exit function variable that is set to os.Exit by default.
+// AssertNoExit asserts that fn does not call the exit function that was passed
+// to NewExit. Usually, exit is a package global function variable that is set
+// to os.Exit by default. The exit function is temporarily modified during the
+// test.
 func (e *Exit) AssertNoExit(fn func()) {
-	e.assert(func(e *Exit) {
-		if e.called {
+	e.assert(func(ex *exiter) {
+		if ex.called {
 			e.t.Error("exit was called")
 		}
 	}, fn)
 }
 
 // assert executes function fn and then calls the check function on the *Exit
-// tester. The check should check the desired state of *Exit after execution of
-// fn, possibly calling t.T.Error() on errors.
-// Panics caused by Exit() are recovered.
-func (e *Exit) assert(check func(*Exit), fn func()) {
-	defer check(e)
+// tester. The check should check the desired state of the exiter after
+// execution of fn, possibly calling e.t.Error() on errors. Panics caused by
+// the exiter are recovered.
+func (e *Exit) assert(check func(*exiter), fn func()) {
+	ex := new(exiter)
+	exitBackup := *e.exit
+	defer func() { *e.exit = exitBackup }()
+	*e.exit = ex.Exit // temporarily record exit calls using the exiter
+
+	defer check(ex)
 
 	panicked := true
 	defer func() {
-		// check that this panic came from Exit() and let it bubble up the stack o/w
-		if panicked && e.called {
+		// check that this panic came from the exiter and let it bubble up the stack o/w
+		if panicked && ex.called {
 			recover()
 		}
 	}()
 
-	e.called = false // reset called state
 	fn()
 	panicked = false
+}
+
+// AssertExit asserts that fn calls the provided exit function with the given
+// code. Usually, exit is a package global function variable that is set to
+// os.Exit by default. The exit function is temporarily modified during the
+// test.
+func AssertExit(t T, exit *func(int), fn func(), code int) {
+	NewExit(t, exit).AssertExit(fn, code)
+}
+
+// AssertNoExit asserts that fn does not call the provided exit function.
+// Usually, exit is a package global function variable that is set to os.Exit by
+// default. The exit function is temporarily modified during the test.
+func AssertNoExit(t T, exit *func(int), fn func()) {
+	NewExit(t, exit).AssertNoExit(fn)
 }
--- pkg/test/exit_test.go
+++ pkg/test/exit_test.go
@@ -13,19 +13,26 @@ import (
 
 func TestExit(t *testing.T) {
 	t.Run("positive tests", func(t *testing.T) {
-		var exit func(int) // use local instead of global exit for this test
+		var code int
+		exit := func(i int) { code = i } // use local instead of global exit for this test
 		et := NewExit(t, &exit)
 		et.AssertExit(func() { exit(42) }, 42)
 		et.AssertNoExit(func() {})
 
+		code = 0
+		exit(17)
+		assert.Equal(t, 17, code,
+			"Exit tester should not permanently modify the exit function")
+
 		called := false
 		et.AssertExit(func() { exit(42); called = true }, 42)
-		assert.False(t, called, "Exit.Exit should pretend further execution")
+		assert.False(t, called, "calling exit should prevent further execution")
+
 		assert.PanicsWithValue(
 			t,
 			"boom",
 			func() {
-				et.assert(func(*Exit) {}, func() { panic("boom") })
+				et.assert(func(*exiter) {}, func() { panic("boom") })
 			},
 			"Exit.assert() should rethrow panic not caused by Exit()",
 		)
@@ -34,13 +41,12 @@ func TestExit(t *testing.T) {
 	t.Run("failing tests", func(t *testing.T) {
 		tt := NewTester(t)
 		var exit func(int) // use local instead of global exit for this test
-		et := NewExit(tt, &exit)
 		// calling AssertExit without exit call should fail
-		tt.AssertError(func(T) { et.AssertExit(func() {}, 42) })
+		tt.AssertError(func(t T) { AssertExit(t, &exit, func() {}, 42) })
 		// calling AssertExit with wrong exit code call should fail
-		tt.AssertError(func(T) { et.AssertExit(func() { exit(53) }, 42) })
+		tt.AssertError(func(t T) { AssertExit(t, &exit, func() { exit(53) }, 42) })
 		// calling AssertNoExit with exit call should fail
-		tt.AssertError(func(T) { et.AssertNoExit(func() { exit(53) }) })
+		tt.AssertError(func(t T) { AssertNoExit(t, &exit, func() { exit(53) }) })
 	})
 
 }
