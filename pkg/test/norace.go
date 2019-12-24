c220e4aeb336931709921aa410b5f27494f29b60
Sebastian Stammler <seb@perun.network>
2019-12-24 10:59:51 +0100

    :sparkles: [pkg/test] Add Race global var to detect -race test
    Signed-off-by: Sebastian Stammler <seb@perun.network>

---
+++ pkg/test/norace.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by a MIT-style license that can be found in
+// the LICENSE file.
+
+// +build !race
+
+package test
+
+// Race tells whether the -race build tag is set
+const Race = false
