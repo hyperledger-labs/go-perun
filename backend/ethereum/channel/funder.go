7187f47e568d47f07441c25cae6fc64f0b33c158
Marius van der Wijden <marius@perun.network>
2020-01-20 14:58:04 +0100

    :recycle: [eth/funder] restructured funder to reduce complexity
    Signed-off-by: Marius van der Wijden <marius@perun.network>

--- backend/ethereum/channel/funder.go
+++ backend/ethereum/channel/funder.go
@@ -15,7 +15,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
 
 	"perun.network/go-perun/backend/ethereum/bindings/assets"
@@ -32,6 +31,7 @@ var (
 type assetHolder struct {
 	*assets.AssetHolder
 	*common.Address
+	assetIndex int
 }
 
 // Funder implements the channel.Funder interface for Ethereum.
@@ -67,70 +67,94 @@ func (f *Funder) Fund(ctx context.Context, request channel.FundingReq) error {
 
 	partIDs := calcFundingIDs(request.Params.Parts, channelID)
 
-	contracts, err := f.connectToContracts(request.Allocation.Assets)
-	if err != nil {
-		return errors.Wrap(err, "Connecting to contracts failed")
+	errChan := make(chan error, len(request.Allocation.Assets))
+	errs := make([]*channel.AssetFundingError, len(request.Allocation.Assets))
+	var wg sync.WaitGroup
+	wg.Add(len(request.Allocation.Assets))
+	for index, asset := range request.Allocation.Assets {
+		go func(index int, asset channel.Asset) {
+			defer wg.Done()
+			contract, err := f.connectToContract(asset, index)
+			if err != nil {
+				errChan <- errors.Wrap(err, "Connecting to contracts failed")
+				return
+			}
+
+			confirmation := make(chan error)
+			go func() {
+				confirmation <- f.waitForFundingConfirmation(ctx, request, contract, partIDs)
+			}()
+
+			if err := f.fundAsset(ctx, request, contract, partIDs); err != nil {
+				errChan <- errors.Wrap(err, "Funding assets failed")
+				return
+			}
+			err = <-confirmation
+			if channel.IsAssetFundingError(err) {
+				errs[index] = err.(*channel.AssetFundingError)
+			} else if err != nil {
+				errChan <- err
+			}
+		}(index, asset)
+	}
+	wg.Wait()
+	close(errChan)
+	if err := <-errChan; err != nil {
+		return err
 	}
 
-	confirmation := make(chan error)
-	go func() {
-		confirmation <- f.waitForFundingConfirmations(ctx, request, contracts, partIDs)
+	var prunedErrs []*channel.AssetFundingError
+	for _, e := range errs {
+		if e != nil {
+			prunedErrs = append(prunedErrs, e)
+		}
+	}
+	if len(prunedErrs) != 0 {
+		return channel.NewFundingTimeoutError(errs)
+	}
+	return nil
+}
+
+func (f *Funder) connectToContract(asset channel.Asset, assetIndex int) (assetHolder, error) {
+	// Decode and set the asset address.
+	assetAddr := asset.(*Asset).Address
+	ctr, err := assets.NewAssetHolder(assetAddr, f)
+	if err != nil {
+		return assetHolder{}, errors.Wrapf(err, "connecting to assetholder")
+	}
+	return assetHolder{ctr, &assetAddr, assetIndex}, nil
+}
+
+func (f *Funder) fundAsset(ctx context.Context, request channel.FundingReq, asset assetHolder, partIDs [][32]byte) error {
+	// Create a new transaction (needs to be cloned because of go-ethereum bug).
+	// See https://github.com/ethereum/go-ethereum/pull/20412
+	balance := new(big.Int).Set(request.Allocation.OfParts[request.Idx][asset.assetIndex])
+	// If we want to fund the channel with ether, send eth in transaction.
+	tx, err := func() (*types.Transaction, error) {
+		f.mu.Lock()
+		defer f.mu.Unlock()
+		var auth *bind.TransactOpts
+		var errI error
+		if bytes.Equal(asset.Bytes(), f.ethAssetHolder.Bytes()) {
+			auth, errI = f.newTransactor(ctx, balance, GasLimit)
+		} else {
+			auth, errI = f.newTransactor(ctx, big.NewInt(0), GasLimit)
+		}
+		if errI != nil {
+			return nil, errors.Wrapf(errI, "creating transactor for asset %d", asset.assetIndex)
+		}
+		// Call the asset holder contract.
+		tx, err := asset.Deposit(auth, partIDs[request.Idx], balance)
+		return tx, errors.WithStack(err)
 	}()
 
-	if err := f.fundAssets(ctx, request, contracts, partIDs); err != nil {
-		return errors.Wrap(err, "Funding assets failed")
+	if err != nil {
+		return errors.WithMessagef(err, "depositing asset %d", asset.assetIndex)
 	}
-
-	return <-confirmation
-}
-
-func (f *Funder) connectToContracts(assetHolders []channel.Asset) ([]assetHolder, error) {
-	contracts := make([]assetHolder, len(assetHolders))
-	// Connect to all AssetHolder contracts.
-	for assetIndex, asset := range assetHolders {
-		// Decode and set the asset address.
-		assetAddr := asset.(*Asset).Address
-		ctr, err := assets.NewAssetHolder(assetAddr, f)
-		if err != nil {
-			return nil, errors.Wrapf(err, "connecting to assetholder %d", assetIndex)
-		}
-		contracts[assetIndex] = assetHolder{ctr, &assetAddr}
+	if err := execSuccessful(ctx, f.ContractBackend, tx); err != nil {
+		return errors.WithMessage(err, "mining transaction")
 	}
-	return contracts, nil
-}
-
-func (f *Funder) fundAssets(ctx context.Context, request channel.FundingReq, contracts []assetHolder, partIDs [][32]byte) (err error) {
-	// Connect to all AssetHolder contracts.
-	for assetIndex, asset := range contracts {
-		// Create a new transaction (needs to be cloned because of go-ethereum bug).
-		// See https://github.com/ethereum/go-ethereum/pull/20412
-		balance := new(big.Int).Set(request.Allocation.OfParts[request.Idx][assetIndex])
-		var auth *bind.TransactOpts
-		// If we want to fund the channel with ether, send eth in transaction.
-		tx, err := func() (*types.Transaction, error) {
-			f.mu.Lock()
-			defer f.mu.Unlock()
-			if bytes.Equal(asset.Bytes(), f.ethAssetHolder.Bytes()) {
-				auth, err = f.newTransactor(ctx, balance, GasLimit)
-			} else {
-				auth, err = f.newTransactor(ctx, big.NewInt(0), GasLimit)
-			}
-			if err != nil {
-				return nil, errors.Wrapf(err, "creating transactor for asset %d", assetIndex)
-			}
-			// Call the asset holder contract.
-			tx, err := contracts[assetIndex].Deposit(auth, partIDs[request.Idx], balance)
-			return tx, errors.WithStack(err)
-		}()
-
-		if err != nil {
-			return errors.WithMessagef(err, "depositing asset %d", assetIndex)
-		}
-		if err := execSuccessful(ctx, f.ContractBackend, tx); err != nil {
-			return errors.WithMessage(err, "mining transaction")
-		}
-		f.log.Debugf("peer[%d] Sending transaction to the blockchain with txHash: %v, amount %d", request.Idx, tx.Hash().Hex(), balance)
-	}
+	f.log.Debugf("peer[%d] Sending transaction to the blockchain with txHash: %v, amount %d", request.Idx, tx.Hash().Hex(), balance)
 	return nil
 }
 
@@ -150,52 +174,37 @@ func filterOldEvents(ctx context.Context, asset assetHolder, deposited chan *ass
 	return nil
 }
 
-// waitForFundingConfirmations waits for the confirmations events on the blockchain that
+// waitForFundingConfirmation waits for the confirmation events on the blockchain that
 // both we and all peers successfully funded the channel.
-func (f *Funder) waitForFundingConfirmations(ctx context.Context, request channel.FundingReq, contracts []assetHolder, partIDs [][32]byte) error {
+func (f *Funder) waitForFundingConfirmation(ctx context.Context, request channel.FundingReq, asset assetHolder, partIDs [][32]byte) error {
 	deposited := make(chan *assets.AssetHolderDeposited)
-	subs := make([]event.Subscription, 0, len(contracts))
-	defer func() {
-		for _, sub := range subs {
-			sub.Unsubscribe()
-		}
-	}()
-
-	// Wait for confirmation on each asset.
-	for assetIndex := range contracts {
-		// Watch new events
-		watchOpts, err := f.newWatchOpts(ctx)
-		if err != nil {
-			return errors.WithMessage(err, "error creating watchopts")
-		}
-		sub, err := contracts[assetIndex].WatchDeposited(watchOpts, deposited, partIDs)
-		if err != nil {
-			return errors.Wrapf(err, "WatchDeposit on asset %d failed", assetIndex)
-		}
-		subs = append(subs, sub)
+	// Watch new events
+	watchOpts, err := f.newWatchOpts(ctx)
+	if err != nil {
+		return errors.WithMessage(err, "error creating watchopts")
 	}
+	sub, err := asset.WatchDeposited(watchOpts, deposited, partIDs)
+	if err != nil {
+		return errors.Wrapf(err, "WatchDeposit on asset %d failed", asset.assetIndex)
+	}
+	defer sub.Unsubscribe()
 
 	// we let the filter queries and all subscription errors write into this error
 	// channel.
-	errChan := make(chan error, len(contracts)+1)
-	for i := 0; i < len(contracts); i++ {
-		go func(i int) {
-			errChan <- errors.Wrapf(<-subs[i].Err(), "subscription for asset %d", i)
-		}(i)
-	}
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- errors.Wrapf(<-sub.Err(), "subscription for asset %d", asset.assetIndex)
+	}()
 
 	// Query old events
 	go func() {
-		for i, c := range contracts {
-			if err := filterOldEvents(ctx, c, deposited, partIDs); err != nil {
-				errChan <- errors.WithMessagef(err, "filtering old Deposited events for asset %d", i)
-				return
-			}
+		if err := filterOldEvents(ctx, asset, deposited, partIDs); err != nil {
+			errChan <- errors.WithMessagef(err, "filtering old Deposited events for asset %d", asset.assetIndex)
 		}
 	}()
 
 	allocation := request.Allocation.Clone()
-	N := len(contracts) * len(request.Params.Parts)
+	N := len(request.Params.Parts)
 	for N > 0 {
 		select {
 		case event := <-deposited:
@@ -211,43 +220,34 @@ func (f *Funder) waitForFundingConfirmations(ctx context.Context, request channe
 				}
 			}
 
-			// Retrieve the position in the asset array.
-			assetIdx := -1
-			for h, ctr := range contracts {
-				if *ctr.Address == event.Raw.Address {
-					assetIdx = h
-					break
-				}
-			}
-
-			amount := allocation.OfParts[idx][assetIdx]
-			if amount.Cmp(big.NewInt(0)) == 0 {
+			amount := allocation.OfParts[idx][asset.assetIndex]
+			if amount.Sign() == 0 {
 				continue // ignore double events
 			}
 
-			log.Debugf("Deposited event received for asset %d and participant %d", assetIdx, idx)
+			log.Debugf("Deposited event received for asset %d and participant %d", asset.assetIndex, idx)
 
 			amount.Sub(amount, event.Amount)
 			if amount.Sign() != 1 {
+				// participant funded successfully
 				N--
+				allocation.OfParts[idx][asset.assetIndex] = big.NewInt(0)
 			}
 
 		case <-ctx.Done():
-			var errors []channel.AssetFundingError
-			for i := 0; i < len(allocation.OfParts); i++ {
-				var indices []channel.Index
-				for k := 0; k < len(allocation.OfParts[i]); k++ {
-					if allocation.OfParts[i][k].Sign() == 1 {
-						indices = append(indices, channel.Index(i))
-					}
+			var indices []channel.Index
+			for k := 0; k < len(allocation.OfParts[asset.assetIndex]); k++ {
+				if allocation.OfParts[asset.assetIndex][k].Sign() == 1 {
+					indices = append(indices, channel.Index(k))
 				}
-				errors = append(errors, channel.AssetFundingError{Asset: i, TimedOutPeers: indices})
 			}
-			return channel.NewFundingTimeoutError(errors)
+			if indices != nil {
+				return channel.AssetFundingError{Asset: asset.assetIndex, TimedOutPeers: indices}
+			}
+			return nil
 		case err := <-errChan:
 			return err
 		}
 	}
-
 	return nil
 }
