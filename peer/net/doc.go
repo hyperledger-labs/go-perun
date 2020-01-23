32a375829318c4fde23705feb33bc7ae95db0ca1
Steffen Rattay <steffen@perun.network>
2020-01-23 15:01:08 +0100

    :bulb: [peer/net] Added doc.go.
    Contains package documentation and import path comment.  
      
    Signed-off-by: Steffen Rattay <steffen@perun.network>

---
+++ peer/net/doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2020 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by a MIT-style license that can be found in
+// the LICENSE file.
+
+// Package net contains a Dialer and Listener implementation for connecting
+// peers over TCP, UDP, and Unix sockets.
+package net // import "perun.network/go-perun/peer/net"
