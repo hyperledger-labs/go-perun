efeb345c59f22974d75e18a0ca47d4136c96dd9a
Marius van der Wijden <marius@perun.network>
2020-01-30 14:28:41 +0100

    :sparkles: [eth/channel] implemented register and refute
    Signed-off-by: Marius van der Wijden <marius@perun.network>

--- backend/ethereum/channel/register.go
+++ backend/ethereum/channel/register.go
@@ -5,16 +5,45 @@
 
 package channel // import "perun.network/go-perun/backend/ethereum/channel"
 
-import "context"
+import (
+	"context"
+	"math/big"
 
-import "perun.network/go-perun/channel"
+	"github.com/pkg/errors"
+	"perun.network/go-perun/channel"
+	"perun.network/go-perun/log"
+)
 
-import "errors"
-
-func (a *Adjudicator) register(ctx context.Context, request channel.AdjudicatorReq) error {
-	return errors.New("not implemented")
+func (a *Adjudicator) register(ctx context.Context, req channel.AdjudicatorReq) error {
+	ethParams := channelParamsToEthParams(req.Params)
+	ethState := channelStateToEthState(req.Tx.State)
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	trans, err := a.newTransactor(ctx, big.NewInt(0), GasLimit)
+	if err != nil {
+		return errors.WithMessage(err, "creating transactor")
+	}
+	tx, err := a.contract.Register(trans, ethParams, ethState, req.Tx.Sigs)
+	if err != nil {
+		return errors.Wrap(err, "calling concludeFinal")
+	}
+	log.Debugf("Sending transaction to the blockchain with txHash: %v", tx.Hash().Hex())
+	return execSuccessful(ctx, a.ContractBackend, tx)
 }
 
-func (a *Adjudicator) refute(ctx context.Context, request channel.AdjudicatorReq) error {
-	return errors.New("not implemented")
+func (a *Adjudicator) refute(ctx context.Context, req channel.AdjudicatorReq) error {
+	ethParams := channelParamsToEthParams(req.Params)
+	ethState := channelStateToEthState(req.Tx.State)
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	trans, err := a.newTransactor(ctx, big.NewInt(0), GasLimit)
+	if err != nil {
+		return errors.WithMessage(err, "creating transactor")
+	}
+	tx, err := a.contract.Refute(trans, ethParams, ethState, req.Tx.Sigs)
+	if err != nil {
+		return errors.Wrap(err, "calling concludeFinal")
+	}
+	log.Debugf("Sending transaction to the blockchain with txHash: %v", tx.Hash().Hex())
+	return execSuccessful(ctx, a.ContractBackend, tx)
 }
