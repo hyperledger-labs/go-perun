6219917d470e2a9ddd60ef44d615ab865b08202a
Steffen Rattay <steffen@perun.network>
2020-03-23 16:19:50 +0100

    :bug: [sim] Fixed package name.
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- backend/sim/init.go
+++ backend/sim/init.go
@@ -3,7 +3,7 @@
 // of this source code is governed by a MIT-style license that can be found in
 // the LICENSE file.
 
-package channel // import "perun.network/go-perun/backend/sim"
+package sim
 
 import (
 	_ "perun.network/go-perun/backend/sim/channel" // backend init
