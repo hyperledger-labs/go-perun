88aa9b425b330b8918a18b33e1a74645d5ade8c1
Sebastian Stammler <seb@perun.network>
2020-05-06 13:08:41 +0200

    :bug: [pkg/io/test] remove wrong and superfluous vanity import path
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- pkg/io/test/equal_encoding_test.go
+++ pkg/io/test/equal_encoding_test.go
@@ -3,7 +3,7 @@
 // of this source code is governed by a MIT-style license that can be found in
 // the LICENSE file.
 
-package test // import "perun.network/go-perun/pkg/test"
+package test
 
 import (
 	"math/rand"
