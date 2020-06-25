4f49356c54e7ba3d8b9dd4dafcd7b421ceb3c8e5
Oliver Tale-Yazdi <oliver@perun.network>
2020-06-25 17:10:36 +0000

    :white_check_mark: [wallet] Tested AddrKey
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

--- wallet/address_test.go
+++ wallet/address_test.go
@@ -10,8 +10,11 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	_ "perun.network/go-perun/backend/sim/wallet"
 	iotest "perun.network/go-perun/pkg/io/test"
+	pkgtest "perun.network/go-perun/pkg/test"
 	"perun.network/go-perun/wallet"
 	wallettest "perun.network/go-perun/wallet/test"
 )
@@ -39,4 +42,41 @@ func TestAddresses_Serializer(t *testing.T) {
 
 	addrs = wallettest.NewRandomAddresses(rng, 5)
 	iotest.GenericSerializerTest(t, &testAddresses{addrs})
+}
+
+func TestAddrKey_Equals(t *testing.T) {
+	rng := pkgtest.Prng(t)
+	addrs := wallettest.NewRandomAddresses(rng, 10)
+
+	// Test all properties of an equivalence relation.
+	for i, a := range addrs {
+		for j, b := range addrs {
+			// Symmetry.
+			require.Equal(t, wallet.Key(a).Equals(b), wallet.Key(b).Equals(a))
+			// Test that Equals is equivalent to ==.
+			require.Equal(t, wallet.Key(a).Equals(b), wallet.Key(a) == wallet.Key(b))
+			// Test that it is not trivially set to true or false.
+			require.Equal(t, i == j, wallet.Key(a).Equals(b))
+			// Transitivity.
+			for _, c := range addrs {
+				if wallet.Key(a).Equals(b) && wallet.Key(b).Equals(c) {
+					require.True(t, wallet.Key(a).Equals(c))
+				}
+			}
+		}
+		// Reflexivity.
+		require.True(t, wallet.Key(a).Equals(a))
+	}
+}
+
+func TestAddrKey(t *testing.T) {
+	rng := pkgtest.Prng(t)
+	addrs := wallettest.NewRandomAddresses(rng, 10)
+
+	for _, a := range addrs {
+		// Test that Key and FromKey are dual to each other.
+		require.Equal(t, wallet.Key(a), wallet.Key(wallet.FromKey(wallet.Key(a))))
+		// Test that FromKey returns the correct Address.
+		require.True(t, a.Equals(wallet.FromKey(wallet.Key(a))))
+	}
 }
