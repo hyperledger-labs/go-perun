53ee865cbf6cbc913f87afaca2290618e35391b4
Steffen Rattay <steffen@perun.network>
2020-07-09 17:53:39 +0200

    :white_check_mark: [wire] Added LocalBus test.
    Signed-off-by: Steffen Rattay <steffen@perun.network>

---
+++ wire/localbus_test.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by the Apache 2.0 license that can be found
+// in the LICENSE file.
+
+package wire_test
+
+import (
+	"testing"
+
+	"perun.network/go-perun/wire"
+	"perun.network/go-perun/wire/test"
+)
+
+func TestLocalBus(t *testing.T) {
+	test.GenericBusTest(t, wire.NewLocalBus(), 16, 10)
+}
