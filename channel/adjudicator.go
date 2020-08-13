6f93a068b00f184d20d85ecdf4c8a4c40447bdd5
Sebastian Stammler <seb@perun.network>
2020-08-13 18:01:35 +0200

    :sparkles: [channel] Add optional Secondary flag to AdjudicatorReq.
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- channel/adjudicator.go
+++ channel/adjudicator.go
@@ -57,11 +57,17 @@ type (
 
 	// An AdjudicatorReq collects all necessary information to make calls to the
 	// adjudicator.
+	//
+	// If the Secondary flag is set to true, it is assumed that this is an
+	// on-chain request that is executed by the other channel participants as well
+	// and the Adjudicator backend may run an optimized on-chain transaction
+	// protocol, possibly saving unnecessary double sending of transactions.
 	AdjudicatorReq struct {
-		Params *Params
-		Acc    wallet.Account
-		Tx     Transaction
-		Idx    Index
+		Params    *Params
+		Acc       wallet.Account
+		Tx        Transaction
+		Idx       Index
+		Secondary bool
 	}
 
 	// RegisteredEvent is the abstract event that signals a successful state
