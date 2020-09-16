3715b0ca1dd13eafc1e62bc3029089bd0eec116a
Steffen Rattay <steffen@perun.network>
2020-09-16 13:42:57 +0200

    :recycle: [pkg/errors] Improved Gatherer API.
    Changed Wait() to return Err() at the end.
    Also simplified the accumulatedError type.
    
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- pkg/errors/gatherer_test.go
+++ pkg/errors/gatherer_test.go
@@ -19,6 +19,8 @@ import (
 	"testing"
 	"time"
 
+	pkgerrors "github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"perun.network/go-perun/pkg/context/test"
@@ -53,9 +55,10 @@ func TestGatherer_Go_and_Wait(t *testing.T) {
 		return stderrors.New("")
 	})
 
-	test.AssertNotTerminates(t, timeout/2, g.Wait)
-	test.AssertTerminates(t, timeout, g.Wait)
-	require.Error(t, g.Err())
+	test.AssertNotTerminates(t, timeout/2, func() { g.Wait() })
+	var err error
+	test.AssertTerminates(t, timeout, func() { err = g.Wait() })
+	require.Error(t, err)
 }
 
 func TestGatherer_Add_and_Err(t *testing.T) {
@@ -81,6 +84,11 @@ func TestCauses(t *testing.T) {
 
 	g.Add(stderrors.New("3"))
 	require.Len(t, errors.Causes(g.Err()), 3)
+
+	err := stderrors.New("normal")
+	causes := errors.Causes(err)
+	require.Len(t, causes, 1)
+	assert.Same(t, causes[0], err)
 }
 
 func TestAccumulatedError_Error(t *testing.T) {
@@ -90,4 +98,17 @@ func TestAccumulatedError_Error(t *testing.T) {
 
 	g.Add(stderrors.New("2"))
 	require.Equal(t, g.Err().Error(), "(2 errors)\n1): 1\n2): 2")
+}
+
+type stackTracer interface {
+	StackTrace() pkgerrors.StackTrace
+}
+
+func TestAccumulatedError_StackTrace(t *testing.T) {
+	g := errors.NewGatherer()
+
+	g.Add(stderrors.New("1"))
+	assert.Nil(t, g.Err().(stackTracer).StackTrace())
+	g.Add(pkgerrors.New("2"))
+	assert.NotNil(t, g.Err().(stackTracer).StackTrace())
 }
