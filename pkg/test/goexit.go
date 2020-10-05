36c96f0cb6aea8e7d27697b1e663dc9a39fd948d
Steffen Rattay <steffen@perun.network>
2020-10-05 19:37:11 +0200

    :sparkles: [pkg/test] Added CheckAbort() and Abort interface.
    - CheckGoexit now relays panics instead of blocking them.
    
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- pkg/test/goexit.go
+++ pkg/test/goexit.go
@@ -14,24 +14,158 @@
 
 package test
 
-// CheckGoexit tests whether a supplied function calls runtime.Goexit during its
-// execution.
-// Returns whether the supplied function did call runtime.Goexit.
-func CheckGoexit(function func()) bool {
+import (
+	"fmt"
+	"runtime/debug"
+	"strings"
+)
+
+// Abort describes the reason for an extraordinary function termination. It is
+// either of type Panic or Goexit, or nil.
+type Abort interface {
+	// Stack returns the stack trace of the termination's cause.
+	Stack() string
+	// String returns a textual representation of the Abort cause.
+	String() string
+}
+
+type abortBase struct {
+	stack string
+}
+
+func (a abortBase) Stack() string {
+	return a.stack
+}
+
+// Panic describes a recovered runtime.Goexit() or panic(), containing the
+// original message (in case of a panic) and the stack trace that caused the panic().
+type Panic struct {
+	abortBase
+	value interface{} // The argument to panic() that was used.
+}
+
+// String formats the abort so that it can be printed similar to the native
+// panic printing.
+func (p Panic) String() string {
+	return fmt.Sprintf("panic: %v\n\n%s", p.value, p.stack)
+}
+
+// Value returns the value that was passed to Panic().
+func (p Panic) Value() interface{} {
+	return p.value
+}
+
+// Goexit describes a recovered runtime.Goexit().
+type Goexit struct {
+	abortBase
+}
+
+func (g Goexit) String() string {
+	return "runtime.Goexit:\n\n" + g.Stack()
+}
+
+// CheckAbort tests whether a supplied function is aborted early using panic()
+// or runtime.Goexit(). Returns a descriptor of the termination cause or nil if
+// it terminated normally.
+func CheckAbort(function func()) (abort Abort) {
 	done := make(chan struct{})
-	goexit := true
+
+	goexit := true  // Whether runtime.Goexit occurred.
+	aborted := true // Whether panic or runtime.Goexit occurred.
+
+	var base abortBase        // The abort cause's stack trace.
+	var recovered interface{} // The recovered panic() value.
+
 	go func() {
 		defer close(done)
 		func() {
-			defer func() { recover() }()
+			defer func() {
+				if aborted {
+					// Recover the panic's value, and if it was a Panic already,
+					// do not wrap it again.
+					recovered = recover()
+					if p, ok := recovered.(*Panic); ok {
+						base.stack = p.Stack()
+						recovered = p.Value()
+					} else {
+						// Hide all mentions of CheckAbort and its inner
+						// functions as well as panic and Goexit.
+						base.stack = getStack(false, 2, 3)
+					}
+				}
+			}()
 			// Call the function to be checked.
 			function()
+			aborted = false
 		}()
 
-		// This is executed if there was a panic, but no goexit.
+		// This is executed if Goexit was not called.
 		goexit = false
 	}()
 
 	<-done
-	return goexit
+
+	// Concatenate the inner call stack of the failure (which starts at the
+	// goroutine instantiation) with the goroutine that is calling CheckAbort.
+	if goexit || aborted {
+		base.stack += "\n" + getStack(true, 1, 0)
+	}
+
+	if goexit {
+		abort = &Goexit{base}
+	} else if aborted {
+		abort = &Panic{base, recovered}
+	}
+	return
+}
+
+// getStack retrieves the current call stack as text, and optionally removes the
+// first line ("goroutine XXX [running]:") and an optional number of the
+// inner-most and outer-most stack frames.
+func getStack(hideGoroutine bool, hideInnerCallers, hideOuterCallers int) string {
+	goroutine, stack := removeLine(string(debug.Stack()))
+
+	// getStack() + debug.Stack() + hideInnerCallers.
+	removeInnerFunctions := 2 + hideInnerCallers
+	for i := 0; i < 2*removeInnerFunctions; i++ {
+		_, stack = removeLine(stack)
+	}
+
+	for i := 0; i < 2*hideOuterCallers; i++ {
+		stack, _ = removeLastLine(stack)
+	}
+
+	if !hideGoroutine {
+		stack = goroutine + "\n" + stack
+	}
+	return stack
+}
+
+func removeLine(str string) (line, rest string) {
+	i := strings.Index(str, "\n")
+	if i == -1 {
+		i = len(str)
+	}
+	return str[:i], str[i+1:]
+}
+
+func removeLastLine(str string) (start, line string) {
+	str = strings.TrimSuffix(str, "\n") // Ignore newline at end of string.
+	i := strings.LastIndex(str, "\n")
+	if i == -1 {
+		return str, ""
+	}
+	return str[:i], str[i+1:]
+}
+
+// CheckGoexit tests whether a supplied function calls runtime.Goexit during its
+// execution. Rethrows panics, but wrapped into a Panic object to preserve the
+// stack trace and value passed to panic().
+// Returns whether the supplied function did call runtime.Goexit.
+func CheckGoexit(function func()) bool {
+	abort := CheckAbort(function)
+	if p, ok := abort.(*Panic); ok {
+		panic(p)
+	}
+	return abort != nil
 }
--- pkg/test/goexit_test.go
+++ pkg/test/goexit_test.go
@@ -19,11 +19,30 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestCheckAbort(t *testing.T) {
+	abort := CheckAbort(func() {})
+	assert.Nil(t, abort)
+
+	abort = CheckAbort(func() { panic(1) })
+	require.IsType(t, abort, (*Panic)(nil))
+	assert.Equal(t, abort.(*Panic).Value(), 1)
+
+	abort = CheckAbort(func() { panic(nil) })
+	require.IsType(t, abort, (*Panic)(nil))
+	assert.Nil(t, abort.(*Panic).Value())
+
+	abort = CheckAbort(runtime.Goexit)
+	assert.IsType(t, abort, (*Goexit)(nil))
+}
 
 func TestCheckGoexit(t *testing.T) {
 	assert.True(t, CheckGoexit(runtime.Goexit))
-	assert.False(t, CheckGoexit(func() { panic("") }))
-	assert.False(t, CheckGoexit(func() { panic(nil) }))
+	assert.Panics(t, func() { CheckGoexit(func() { panic("") }) })
+	didPanic, pval := CheckPanic(func() { CheckGoexit(func() { panic(nil) }) })
+	assert.True(t, didPanic)
+	assert.Nil(t, pval)
 	assert.False(t, CheckGoexit(func() {}))
 }
