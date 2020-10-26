73d652ad7588d88deefd7978551c5956ff160798
Matthias Geihs <matthias@perun.network>
2020-10-26 08:59:22 +0100

    :bug: [client/test] Add timeout to two party test
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- client/client_persistence_test.go
+++ client/client_persistence_test.go
@@ -55,7 +55,7 @@ func TestPersistencePetraRobert(t *testing.T) {
 			TxAmounts:   [2]*big.Int{big.NewInt(5), big.NewInt(3)},
 		}
 
-		executeTwoPartyTest(roles, cfg)
+		executeTwoPartyTest(t, roles, cfg)
 	}
 }
 
