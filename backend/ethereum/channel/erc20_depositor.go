cf68fafc9fa21ee1913268548edff80492b7792c
Oliver Tale-Yazdi <oliver@perun.network>
2020-10-27 17:47:38 +0100

    :tada: [eth/channel] Add ERC20-Depositor.
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

---
+++ backend/ethereum/channel/erc20_depositor.go
@@ -0,0 +1,66 @@
+// Copyright 2020 - See NOTICE file for copyright holders.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package channel
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pkg/errors"
+
+	"perun.network/go-perun/backend/ethereum/bindings/assets"
+)
+
+// ERC20Depositor deposits tokens into the `AssetHolderERC20` contract.
+// It is bound to a token but can be reused to deposit multiple times.
+type ERC20Depositor struct {
+	Token common.Address
+}
+
+// ERC20DepositorTXGasLimit is the limit of Gas that an `ERC20Depositor` will
+// spend per transaction when depositing funds. It is set to 100k.
+// An `IncreaseAllowance` uses ~45kGas and a `Deposit` call ~84kGas on average.
+const ERC20DepositorTXGasLimit = 100000
+
+// Deposit returns the transactions for despositing Ethereum or an error otherwise.
+func (d *ERC20Depositor) Deposit(ctx context.Context, req DepositReq) ([]*types.Transaction, error) {
+	// Bind a `AssetHolderERC20` instance.
+	assetholder, err := assets.NewAssetHolderERC20(common.Address(req.Asset), req.CB)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "binding AssetHolderERC20 contract at: %x", req.Asset)
+	}
+	// Bind an `ERC20` instance.
+	token, err := assets.NewERC20(d.Token, req.CB)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "binding ERC20 contract at: %x", d.Token)
+	}
+	// Increase the allowance.
+	opts, err := req.CB.NewTransactor(ctx, nil /*Don't send ETH*/, ERC20DepositorTXGasLimit, req.Account)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "creating transactor for asset: %x", req.Asset)
+	}
+	tx1, err := token.IncreaseAllowance(opts, common.Address(req.Asset), req.Balance)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "increasing allowance for asset: %x", req.Asset)
+	}
+	// Deposit.
+	opts, err = req.CB.NewTransactor(ctx, nil /*Don't send ETH*/, ERC20DepositorTXGasLimit, req.Account)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "creating transactor for asset: %x", req.Asset)
+	}
+	tx2, err := assetholder.Deposit(opts, req.FundingID, req.Balance)
+	return []*types.Transaction{tx1, tx2}, errors.WithMessage(err, "AssetHolderERC20 depositing")
+}
