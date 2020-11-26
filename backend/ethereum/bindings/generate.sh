8d45d981a2892675df974c024904a6fb990c4414
Matthias Geihs <matthias@perun.network>
2020-11-26 15:40:36 +0100

    :construction_worker: [backend/ethereum/bindings] Revise generator script
    - Add compatibility for Darwin OS and Unix shell.
    - Separate contracts into different packages
    
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- backend/ethereum/bindings/generate.sh
+++ backend/ethereum/bindings/generate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright 2020 - See NOTICE file for copyright holders.
 #
@@ -16,38 +16,64 @@
 
 set -e
 
+ABIGEN=abigen
+SOLC=solc
+
 # Download solc.
-wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-static-linux"
-chmod +x solc-static-linux
-echo -e "Exec 'git submodule update --init --recursive' once after cloning."
-echo -e "Ensure that the newest version of abigen is installed"
+if [ `uname` == "Linux" ]; then
+    # GNU Linux
+    wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-static-linux"
+    chmod +x solc-static-linux
+    SOLC=./solc-static-linux
+
+elif [ `uname` == "Darwin" ]; then
+    # Mac OSX
+    curl -L "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-macos" -o solc-macos
+    chmod +x solc-macos
+    SOLC=./solc-macos
+
+else
+    # Unsupported
+    echo "Unsupported operating system: `uname`. Exiting."
+    exit 1
+fi
+
+echo "Exec 'git submodule update --init --recursive' once after cloning."
+echo "Ensure that the newest version of abigen is installed."
 
 # Generates optimized golang bindings and runtime binaries for sol contracts.
 # $1  solidity file path, relative to ../contracts/contracts/.
-# $1  golang package name.
-# $2… list of contract names.
+# $2  golang package name.
 function generate() {
-    file=$1; pkg=$2
-    shift; shift   # skip the first two args.
-    for contract in "$@"; do
-        abigen --pkg $pkg --sol ../contracts/contracts/$file.sol --out $pkg/$file.go --solc ./solc-static-linux
-        ./solc-static-linux --bin-runtime --optimize --allow-paths *, ../contracts/contracts/$file.sol --overwrite -o $pkg/
-        echo -e "package $pkg\n\n // ${contract}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts.\nvar ${contract}BinRuntime = \`$(<${pkg}/${contract}.bin-runtime)\`" > "$pkg/${contract}BinRuntime.go"
-    done
+    FILE=$1; PKG=$2; CONTRACT=$FILE
+    echo "generate package $PKG"
+
+    mkdir -p $PKG
+
+    # generate abi
+    $ABIGEN --pkg $PKG --sol ../contracts/contracts/$FILE.sol --out $PKG/$FILE.go --solc $SOLC
+
+    # generate go bindings
+    $SOLC --bin-runtime --optimize --allow-paths *, ../contracts/contracts/$FILE.sol --overwrite -o $PKG/
+
+    # generate binary runtime
+    BIN_RUNTIME=`cat ${PKG}/${CONTRACT}.bin-runtime`
+    OUT_FILE="$PKG/${CONTRACT}BinRuntime.go"
+    echo "package $PKG // import \"perun.network/go-perun/backend/ethereum/bindings/$PKG\"" > $OUT_FILE
+    echo >> $OUT_FILE
+    echo "// ${CONTRACT}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts." >> $OUT_FILE
+    echo "var ${CONTRACT}BinRuntime = \"$BIN_RUNTIME\"" >> $OUT_FILE
 }
 
 # Adjudicator
-generate "Adjudicator" "adjudicator" "Adjudicator"
+generate "Adjudicator" "adjudicator"
 
-# PerunToken, AssetHolderETH and AssetHolderERC20
-# Pragma statements can only be in a solidity file once, so
-# we remove the duplicates with awk.
-cat ../contracts/contracts/PerunToken.sol \
-    <(awk '/^pragma/{p=1;next}{if(p){print}}' ../contracts/contracts/AssetHolderETH.sol) \
-    <(awk '/^pragma/{p=1;next}{if(p){print}}' ../contracts/contracts/AssetHolderERC20.sol) \
-    > ../contracts/contracts/Contracts.sol
-generate "Contracts" "assets" "PerunToken" "AssetHolderETH" "AssetHolderERC20"
-rm ../contracts/contracts/Contracts.sol
+# Asset holders
+generate "AssetHolder" "assetholder"
+generate "AssetHolderETH" "assetholdereth"
+generate "AssetHolderERC20" "assetholdererc20"
 
-abigen --version --solc ./solc-static-linux
-echo -e "Generated bindings"
+# Tokens
+generate "PerunToken" "peruntoken"
+
+echo "Generated bindings"
