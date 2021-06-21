7d55d71c868f4cc632cbd73b90e4b7269ce6a853
Matthias Geihs <matthias@perun.network>
2021-06-21 17:38:38 +0200

    :children_crossing: [client] Always settle with sub-channels
    Usage before was clunky. The user needed to gather subchannels manually
    and to know which function to call. Now the framework does that for the user.
    
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- client/client_role_test.go
+++ client/client_role_test.go
@@ -54,6 +54,7 @@ func NewSetups(rng *rand.Rand, names []string) []ctest.RoleSetup {
 			Adjudicator: backend,
 			Wallet:      wtest.NewWallet(),
 			Timeout:     roleOperationTimeout,
+			Backend:     backend,
 		}
 	}
 
