5d5754cf7fd88bdc91c44a6677d541d8cf9d9804
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2021-09-09 17:21:39 +0530

    :sparkles: [watcher/local] Implement start watching functionality
    - Implementation is same for ledger and non-ledger channels
      (sub/virtual).
    
    - However, since non-ledger channel require an additional parameter
      (parent), separate API interfaces are provided.
    
    - Mocks (placed in watcher/internal/mocks) were generated using testify
      framework.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- watcher/local/watcher.go
+++ watcher/local/watcher.go
@@ -15,12 +15,92 @@
 package local
 
 import (
+	"context"
+	"sync"
+
+	"github.com/pkg/errors"
+
 	"perun.network/go-perun/channel"
+	"perun.network/go-perun/watcher"
 )
 
 type (
+	// Watcher implements a local watcher.
+	Watcher struct {
+		rs channel.RegisterSubscriber
+		*registry
+	}
+
+	// Config represents the configuration for initializing a local watcher.
+	Config struct {
+		RegisterSubscriber channel.RegisterSubscriber
+	}
+
 	ch struct {
 		id     channel.ID
+		params *channel.Params
 		parent channel.ID
+
+		subChsAccess sync.Mutex
 	}
 )
+
+// NewWatcher initializes a local watcher.
+//
+// Local watcher implements pub-sub interfaces over "go channels".
+func NewWatcher(cfg Config) (*Watcher, error) {
+	w := &Watcher{
+		rs:       cfg.RegisterSubscriber,
+		registry: newRegistry(),
+	}
+	return w, nil
+}
+
+// StartWatchingLedgerChannel starts watching for a ledger channel.
+func (w *Watcher) StartWatchingLedgerChannel(ctx context.Context, signedState channel.SignedState) (
+	watcher.StatesPub, watcher.AdjudicatorSub, error) {
+	return w.startWatching(ctx, channel.Zero, signedState)
+}
+
+// StartWatchingSubChannel starts watching for a sub-channel or virtual channel.
+func (w *Watcher) StartWatchingSubChannel(ctx context.Context, parent channel.ID, signedState channel.SignedState) (
+	watcher.StatesPub, watcher.AdjudicatorSub, error) {
+	parentCh, ok := w.registry.retrieve(parent)
+	if !ok {
+		return nil, nil, errors.New("parent channel not registered with the watcher")
+	}
+	parentCh.subChsAccess.Lock()
+	defer parentCh.subChsAccess.Unlock()
+	return w.startWatching(ctx, parent, signedState)
+}
+
+func (w *Watcher) startWatching(ctx context.Context, parent channel.ID, signedState channel.SignedState) (
+	watcher.StatesPub, watcher.AdjudicatorSub, error) {
+	id := signedState.State.ID
+	w.registry.lock()
+	defer w.registry.unlock()
+
+	if _, ok := w.registry.retrieveUnsafe(id); ok {
+		return nil, nil, errors.New("already watching for this channel")
+	}
+
+	_, err := w.rs.Subscribe(ctx, id)
+	if err != nil {
+		return nil, nil, errors.WithMessage(err, "subscribing to adjudicator events from blockchain")
+	}
+	statesPubSub := newStatesPubSub()
+	eventsToClientPubSub := newAdjudicatorEventsPubSub()
+	ch := newCh(id, parent, signedState.Params)
+
+	w.registry.addUnsafe(ch)
+
+	return statesPubSub, eventsToClientPubSub, nil
+}
+
+func newCh(id, parent channel.ID, params *channel.Params) *ch {
+	return &ch{
+		id:     id,
+		params: params,
+		parent: parent,
+	}
+}
