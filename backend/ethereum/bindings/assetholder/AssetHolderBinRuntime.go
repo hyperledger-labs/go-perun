103e91a2c9db2e259f295af4139cbb6f7b026ec7
Matthias Geihs <matthias@perun.network>
2022-03-18 11:00:55 +0100

    Remove Ethereum backend
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- backend/ethereum/bindings/assetholder/AssetHolderBinRuntime.go
+++
@@ -1,4 +0,0 @@
-package assetholder // import "perun.network/go-perun/backend/ethereum/bindings/assetholder"
-
-// AssetHolderBinRuntime is the runtime part of the compiled bytecode used for deploying new contracts.
-var AssetHolderBinRuntime = ""
