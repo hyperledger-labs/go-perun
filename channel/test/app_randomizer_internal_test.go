30ac36a79baa91377e6bc1eb4981dd27a3c8b45b
Minh Huy Tran <huy.tranminh2804@gmail.com>
2024-02-06 12:02:46 +0100

    fix(app_randomizer_internal_test): add identifier for mock_app_randomizer to recognise change
    Signed-off-by: Minh Huy Tran <huy.tranminh2804@gmail.com>

--- channel/test/app_randomizer_internal_test.go
+++ channel/test/app_randomizer_internal_test.go
@@ -29,13 +29,13 @@ func TestAppRandomizerSet(t *testing.T) {
 	assert.False(t, isAppRandomizerSet, "isAppRandomizerSet should be defaulted to false")
 
 	old := appRandomizer
-	assert.NotPanics(t, func() { SetAppRandomizer(&MockAppRandomizer{}) }, "first SetAppRandomizer() should work")
+	assert.NotPanics(t, func() { SetAppRandomizer(NewMockAppRandomizer()) }, "first SetAppRandomizer() should work")
 	assert.True(t, isAppRandomizerSet, "isAppRandomizerSet should be true")
 	assert.NotNil(t, appRandomizer, "appRandomizer should not be nil")
 	assert.False(t, old == appRandomizer, "appRandomizer should have changed")
 
 	old = appRandomizer
-	assert.Panics(t, func() { SetAppRandomizer(&MockAppRandomizer{}) }, "second SetAppRandomizer() should panic")
+	assert.Panics(t, func() { SetAppRandomizer(NewMockAppRandomizer()) }, "second SetAppRandomizer() should panic")
 	assert.True(t, isAppRandomizerSet, "isAppRandomizerSet should be true")
 	assert.NotNil(t, appRandomizer, "appRandomizer should not be nil")
 	assert.True(t, old == appRandomizer, "appRandomizer should not have changed")
